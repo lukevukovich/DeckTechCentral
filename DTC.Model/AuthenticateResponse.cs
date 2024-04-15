406da851591aaac6720187f8bbabb2394e2e3c12
Chremmer <barker8101@gmail.com>
2024-04-15 13:02:57 -0400

    specified the json names of authenticate response

--- DTC.Model/AuthenticateResponse.cs
+++ DTC.Model/AuthenticateResponse.cs
@@ -1,12 +1,18 @@
 using System.Security.Principal;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace DTC.Model
 {
     public class AuthenticateResponse
     {
+        [JsonPropertyName("id")]
         public Guid Id { get; set; }
+        [JsonPropertyName("username")]
         public string Username { get; set; }
+        [JsonPropertyName("email")]
         public string Email { get; set; }
+        [JsonPropertyName("token")]
         public string Token { get; set; }
 
         public AuthenticateResponse(User user, string token) {
